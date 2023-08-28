3b02461f1714893b98da1ce4d386ad2b05bc7700
unknown <cristian.rodriguez-g@labinfo.is.escuelaing.edu.co>
2023-08-28 12:29:26 -0500

    html & js

---
+++ target/classes/color.js
@@ -0,0 +1,9 @@
+var color = function (){
+    background = document.querySelector("h1")
+    if (background.style.color == "blue"){
+        background.style.color = "red";
+    }else{
+        background.style.color = "blue";
+    }
+
+}
---
+++ target/classes/prueba.js
@@ -0,0 +1,4 @@
+function generateRandomCode() {
+     var myRandomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+      return myRandomColor;
+   }
